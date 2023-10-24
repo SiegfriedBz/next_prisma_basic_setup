d0a9739178e45c9886a1462298ecbbfcf33c9dea
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-24 10:56:34 +0200

    protect posts api route

--- src/pages/api/posts.js
+++ src/pages/api/posts.js
@@ -1,6 +1,17 @@
 import { prisma } from "../../../server/db/client"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "./auth/[...nextauth]"
+
+// Protected API ROUTE
 
 export default async function handle(req, res) {
+  const session = await getServerSession(req, res, authOptions)
+
+  if (!session) {
+    res.status(401).json({ message: "You must be logged in." })
+    return
+  }
+
   const { method } = req
 
   switch (method) {
