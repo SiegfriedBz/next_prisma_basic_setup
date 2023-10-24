690b3d228ab6f366b7dfaa2efab41a93694ea5c1
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-24 10:55:41 +0200

    add SessionProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react"
+import "@/styles/globals.css"
+import Navbar from "@/components/Navbar"
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+config.autoAddCss = false
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Navbar />
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
